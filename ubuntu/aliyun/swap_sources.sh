700f3dea3621e164c2a3f91bb43bbde9e143bc38
ShinChven <shinchven@gmail.com>
2020-05-26 00:02:23 +0800

    add bash script to swap sources

--- ubuntu/aliyun/swap_sources.sh
+++ ubuntu/aliyun/swap_sources.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
 UBUNTU_VERSION="$1"
 
-copy /etc/apt/sources.list /etc/apt/sources.list.bak
+URL="https://raw.githubusercontent.com/ShinChven/mirrors/master/ubuntu/aliyun/$UBUNTU_VERSION.sources.list"
+
+echo $URL
+
+wget $URL -O /etc/apt/sources.list
