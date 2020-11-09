0a47efff921cb0f166b3eed1253e592c6d53656d
ShinChven <shinchven@gmail.com>
2020-11-09 15:54:05 +0800

    update certbot

---
+++ certbot/install.CentOS.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo yum install epel-release
+sudo yum install certbot python2-certbot-apache mod_ssl
---
+++ certbot/install.Ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install certbot python3-certbot-nginx
